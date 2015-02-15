f77df9a295c289b0f8cf1b40c3a5d81cefad6bcc
tsubakimoto <tkmbmbr@gmail.com>
2015-02-15 22:13:47 +0900

    add jenkins-dotnet.test.csproj

---
+++ jenkins-dotnet.test/CalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using jenkins_dotnet;
+using NUnit.Framework;
+using System;
+
+namespace jenkins_dotnet.test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase(1, 2, Result = 3, TestName = "Add01: 1+2=3")]
+        [TestCase(5, 0, Result = 5, TestName = "Add02: 5+0=5")]
+        public int AddTest(int x, int y)
+        {
+            return Calculator.Add(x, y);
+        }
+    }
+}
